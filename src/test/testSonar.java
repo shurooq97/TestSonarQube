9455672db7e292355370f5b551d440ce80e53b8b
hp <hp@shurooqGhaith>
2018-04-02 11:35:44 -0700

    first

---
+++ src/test/testSonar.java
@@ -0,0 +1,14 @@
+package test;
+
+public class testSonar {
+
+	public static int fun(int x,int y){
+		if(x>y)return 1;
+		else return 0;
+		
+	}
+	
+	public static void main(String [] args){
+		System.out.print(fun(2,3));
+	}
+}
